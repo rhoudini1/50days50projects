10d546012dda0a6b4343837b1c7e17b2694b73b0
Renan <araujomonteirogba@hotmail.com>
2022-07-30 23:59:21 -0300

    added transition to active panel h3

---
+++ 01-expanding-cards/script.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
+
+const removeActiveClass = () => {
+  const activePanel = document.querySelector(".active");
+  activePanel.classList.remove("active");
+};
